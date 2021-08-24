5c3ef275485ed83834da81620d1cea84e7a0ce22
MahAmorim <61316782+MahAmorim@users.noreply.github.com>
2021-08-24 20:27:55 -0300

    Update Principal.java

--- HelloWorldOO/src/Principal.java
+++ HelloWorldOO/src/Principal.java
@@ -6,9 +6,7 @@ public class Principal {
     public static void main (String arg[]) {
         //instanciar um obj da classe
         HelloWorldOO hw = new HelloWorldOO();
-        HelloWorldOO hw2 = new HelloWorldOO();
-        
+
         hw.exibe();
-        hw2.exibe();
     }    
 }
