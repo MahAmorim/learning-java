95046f33ae3ec7fed79044d4add7de3929a9227b
MahAmorim <61316782+MahAmorim@users.noreply.github.com>
2021-08-24 20:25:39 -0300

    Add files via upload

---
+++ HelloWorldOO/src/HelloWorldOO.java
@@ -0,0 +1,10 @@
+/**
+ *
+ * @author Marcela Amorim
+ */
+public class HelloWorldOO {
+    void exibe(){
+        System.out.printf("Hello World with OO !!!");
+    }
+    
+}
---
+++ HelloWorldOO/src/Principal.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author Marcela Amorim
+ */
+public class Principal {
+    public static void main (String arg[]) {
+        //instanciar um obj da classe
+        HelloWorldOO hw = new HelloWorldOO();
+        HelloWorldOO hw2 = new HelloWorldOO();
+        
+        hw.exibe();
+        hw2.exibe();
+    }    
+}
